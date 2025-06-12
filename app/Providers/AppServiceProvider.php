b65cfc2569d73565aa216971464d8aec5a16a892
andisjuliansyah <andies.juliansyah@gmail.com>
2025-06-12 20:16:10 +0700

    css custom

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,15 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationItem;
+use Filament\Support\Facades\FilamentAsset;
+use Filament\Support\Assets\Css;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function boot(): void
+    public function boot()
     {
-        //
+        FilamentAsset::register([
+            Css::make('example-local-stylesheet', asset('css/filament-custom.css')),
+        ]);
     }
 }
