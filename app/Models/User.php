ecce15e58d6d228c8ac1eaa1a5d3eafb68b7b638
andisjuliansyah <andies.juliansyah@gmail.com>
2025-08-10 19:25:43 +0700

    Development score

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->hasAnyRole(['Super Admin', 'Editor', 'Juri']);
+        return $this->hasAnyRole(['Super Admin', 'Editor', 'Judge']);
     }
 }
